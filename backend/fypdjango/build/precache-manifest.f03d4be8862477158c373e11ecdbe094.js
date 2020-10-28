a9a5072ddd24a1a73cef0f460b3654a75d0c81d8
Abdullaha042 <72641278+Abdullaha042@users.noreply.github.com>
2020-10-28 22:49:10 +0500

    Add files via upload

---
+++ backend/fypdjango/build/precache-manifest.f03d4be8862477158c373e11ecdbe094.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b730430bfd4df50723667f5d899cb9f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "345232d47c0e09be1cb6",
+    "url": "/static/css/2.e10332d9.chunk.css"
+  },
+  {
+    "revision": "8c97251e77330d0c8e75",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "345232d47c0e09be1cb6",
+    "url": "/static/js/2.6a98bc1e.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.6a98bc1e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c97251e77330d0c8e75",
+    "url": "/static/js/main.07d3a575.chunk.js"
+  },
+  {
+    "revision": "81b3c1e553cc19a9f34f",
+    "url": "/static/js/runtime-main.79f352f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
